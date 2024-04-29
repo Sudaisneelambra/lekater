91f9ce572ec88903e09c47e67f5b055fd2f3c717
Ahammed Shaheer <shaheerbinmuhammed6@gmail.com>
2024-04-29 16:58:10 +0530

    added admin navbar

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    NavBarComponent
   ],
   imports: [
     CommonModule
